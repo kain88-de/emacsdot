4650ac8020e74205192a14b2983b04949b771a40
Max Linke <max_linke@gmx.de>
2015-03-07 18:31:03 +0100

    disbale fci by default

--- config/init-ui.el
+++ config/init-ui.el
@@ -17,10 +17,7 @@
   :init
   (progn
     (setq fci-rulw-width 2)
-    (setq fci-rule-column 80)
-    (define-globalized-minor-mode
-      global-fci-mode fci-mode (lambda () (fci-mode 1)))
-    (global-fci-mode t)))
+    (setq fci-rule-column 80)))
 
 (use-package saveplace
   :init
