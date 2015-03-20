1faa1737364dee0d3132a9df19f7841663c13f7f
Max Linke <max_linke@gmx.de>
2015-03-20 09:28:03 +0100

    make projectile evil

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -8,5 +8,13 @@
 
 ;; this only works outside of use-package investigate
 (eval-after-load 'projectile '(diminish 'projectile-mode))
+;; evil projectile bindings
+(evil-leader/set-key "pp" 'helm-projectile-switch-project)
+(evil-leader/set-key "pf" 'helm-projectile-find-file)
+(evil-leader/set-key "pT" 'helm-projectile-find-test-file)
+(evil-leader/set-key "pb" 'helm-projectile-find-other-file)
+(evil-leader/set-key "pS" 'projectile-save-project-buffers)
+(evil-leader/set-key "pP" 'projectile-test-project)
+(evil-leader/set-key "pc" 'max-save-and-build)
 
 (provide 'init-projectile)
