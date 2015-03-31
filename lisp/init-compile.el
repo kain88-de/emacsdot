0e70a40893f0da3016085d92203983d512e9331c
Max Linke <max_linke@gmx.de>
2015-03-31 21:33:08 +0200

    Update compile function to new emacs usage

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -10,33 +10,6 @@
   (projectile-save-project-buffers)
   (projectile-compile-project t))
 
-(defun max/bury-compile-buffer-if-successful (buffer string)
-  "Bury a compilation buffer if succeeds without warnings "
-  (if (and
-       (string-match "compilation" (buffer-name buffer))
-       (string-match "finished" string)
-       (not
-        (with-current-buffer buffer
-          (search-forward "warning" nil t))))
-      (progn
-        (run-with-timer .2 nil
-                        (lambda (buf)
-                          (bury-buffer buf)
-                          (delete-window (get-buffer-window buf)))
-                        buffer)
-        (message "NO COMPILATION ERRORS"))))
-
-(defun max/new-window-compilation-buffer ()
-  "always use new window for compilation"
-  (when (not (get-buffer-window "*compilation*"))
-    (save-selected-window
-      (save-excursion
-        (let* ((w (split-window-vertically))
-               (h (window-height w)))
-          (select-window w)
-          (switch-to-buffer "*compilation*")
-          (shrink-window (- h 20)))))))
-
 ;; Compilation from Emacs
 (defun max/colorize-compilation-buffer ()
   "Colorize a compilation mode buffer."
@@ -53,9 +26,6 @@
 (add-hook 'compilation-filter-hook #'max/colorize-compilation-buffer)
 (global-set-key (kbd "<f5>") 'max-save-and-build)
 (setq compilation-scroll-output 1)
-
-(add-hook 'compilation-finish-functions 'max/bury-compile-buffer-if-successful)
-;;(add-hook 'compilation-mode-hook 'max/new-window-compilation-buffer)
 
 (evil-leader/set-key "cn" 'next-error)
 (evil-leader/set-key "cp" 'previous-error)
