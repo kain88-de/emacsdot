c92e1dbf8e5a109735a5c8870dc3d5568d11e543
Max Linke <max_linke@gmx.de>
2015-04-04 19:09:19 +0200

    Update projectile mappings

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -7,38 +7,25 @@
   ;; evil projectile bindings
   (evil-leader/set-key "pp" 'helm-projectile-switch-project)
   (evil-leader/set-key "pf" 'helm-projectile-find-file)
-  (evil-leader/set-key "pT" 'helm-projectile-find-test-file)
-  (evil-leader/set-key "pb" 'helm-projectile-find-other-file)
+  (evil-leader/set-key "pF" 'helm-projectile-find-file-in-known-projects)
+  (evil-leader/set-key "pT" 'projectile-find-test-file)
+  (evil-leader/set-key "pa" 'helm-projectile-find-other-file)
+  (evil-leader/set-key "pb" 'helm-projectile-switch-to-buffer)
   (evil-leader/set-key "pS" 'projectile-save-project-buffers)
   (evil-leader/set-key "pP" 'projectile-test-project)
   (evil-leader/set-key "pc" 'max-save-and-build)
-  (evil-leader/set-key "pa" 'helm-projectile-find-other-file)
   (evil-leader/set-key "psa" 'helm-projectile-ack)
+  (evil-leader/set-key "pr" 'projectile-replace)
+  (evil-leader/set-key "pI" 'projectile-ibuffer)
+  (evil-leader/set-key "pg" 'helm-projectile-find-file-dwim)
   :config
   (add-to-list 'projectile-globally-ignored-directories "build")
+  (add-to-list 'projectile-globally-ignored-directories "lin64_build")
   (use-package helm-projectile
     :config
     (setq projectile-completion-system 'helm)
     (helm-projectile-on))
   (projectile-global-mode)
   :diminish projectile-mode)
-
-;; http://emacs.stackexchange.com/questions/10183/close-other-buffers-with-projectile
-(defun max/kill-non-project-buffers (&optional kill-special)
-  "Kill buffers that do not belong to a `projectile' project.
-
-With prefix argument (`C-u'), also kill the special buffers."
-  (interactive "P")
-  (let ((bufs (buffer-list (selected-frame))))
-    (dolist (buf bufs)
-      (with-current-buffer buf
-        (let ((buf-name (buffer-name buf)))
-          (when (or (null (projectile-project-p))
-                    (and kill-special
-                         (string-match "^\*" buf-name)))
-            ;; Preserve buffers with names starting with *scratch or *Messages
-            (unless (string-match "^\\*" buf-name)
-              (message "Killing buffer %s" buf-name)
-              (kill-buffer buf))))))))
 
 (provide 'init-projectile)
