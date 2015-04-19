c6fbb265c758d7c6be98931c4d2bd9af0c4bd619
Max Linke <max_linke@gmx.de>
2015-04-19 21:37:27 +0200

    add org-mode leader bindings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -11,6 +11,10 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 
+(evil-leader/set-key "oa" 'org-agenda)
+(evil-leader/set-key "ol" 'org-store-link)
+(evil-leader/set-key "oe" 'org-export-dispatch)
+
 (setq org-todo-keyword-faces
       '(("TODO" . org-warning) ("STARTED" . "yellow")
         ("CANCELED" . (:foreground "blue" :weight bold))))
