fff2815e934f16bc09187ddd8d5fe38ef7aa0516
Max Linke <max_linke@gmx.de>
2015-04-19 21:37:10 +0200

    reenable guide-key

--- lisp/init-guide-key.el
+++ lisp/init-guide-key.el
@@ -2,9 +2,9 @@
   :ensure t
   :defer t
   :config
-  (progn
-    (setq guide-key/guide-key-sequence t)
-    (guide-key-mode 1)))
+  (setq guide-key/guide-key-sequence t)
+  :init
+  (guide-key-mode 1))
 
 
 (provide 'init-guide-key)
