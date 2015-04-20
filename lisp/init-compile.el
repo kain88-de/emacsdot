c7f7a61360c063d663fbf9e0ece0421d1fdf80c7
Max Linke <max_linke@gmx.de>
2015-04-20 15:58:57 +0200

    Add package to add ENV-VAR in eshell

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -1,18 +1,28 @@
 (use-package compile
   :defer t
-  :init
+  :config
   (setq compilation-ask-about-save nil
         compilation-always-kill t
         compilation-scroll-output 'first-error)
   (evil-leader/set-key "cn" 'next-error)
   (evil-leader/set-key "cp" 'previous-error))
 
+;; extract ENV-VAR to eshell
+;; (use-package exec-path-from-shell
+;;   :ensure t
+;;   :config
+;;   (setq exec-path-from-shell-variables
+;;         (quote
+;;          ("PATH" "MANPATH" "LD_LIBRARY_PATH" "CPLUS_INCLUDE_PATH")))
+;;   :init
+;;   (exec-path-from-shell-initialize))
+
 ;; Colorize output of Compilation Mode, see
 ;; http://stackoverflow.com/a/3072831/355252
 (use-package ansi-color
   :ensure t
   :defer t
-  :init
+  :config
   (defun max/colorize-compilation-buffer ()
     "Colorize a compilation mode buffer."
     (interactive)
