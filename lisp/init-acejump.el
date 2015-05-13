1753874d9e4a4690e997bc950190c3f0c23ed782
Max Linke <max_linke@gmx.de>
2015-05-13 21:57:38 +0200

    acewindow is awesome

--- lisp/init-acejump.el
+++ lisp/init-acejump.el
@@ -1,8 +1,13 @@
 (use-package ace-window
   :ensure t
   :defer t
+  :config (setq aw-dispatch-always t)
   :init
   (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
+  (evil-leader/set-key "wd" 'ace-delete-window)
+  (evil-leader/set-key "ws" 'ace-swap-window)
+  (evil-leader/set-key "wm" 'ace-maximize-window)
+  (evil-leader/set-key "wp" 'ace-window)
   :bind ("M-p" . ace-window))
 
 (use-package avy
