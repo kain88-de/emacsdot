b41281f9869af406e14ea3428811dadbe211d75d
Max Linke <max_linke@gmx.de>
2015-05-13 21:50:05 +0200

    configure abo-abos ace-jump

--- lisp/init-acejump.el
+++ lisp/init-acejump.el
@@ -1,11 +1,16 @@
 (use-package ace-window
   :ensure t
   :defer t
+  :init
+  (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
   :bind ("M-p" . ace-window))
 
 (use-package avy
   :defer t
-  :init (evil-leader/set-key "SPC" 'avy-goto-char)
+  :init
+  (evil-leader/set-key "SPC" 'avy-goto-char)
+  (setq avy-style 'at-full)
+  (setq avy-all-windows nil)
   :bind ("C-x SPC" . avy-goto-char))
 
 (provide 'init-acejump)
