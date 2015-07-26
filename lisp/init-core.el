39e2e814d502b20902184a639bf2f71ed2239b8c
Max Linke <max_linke@gmx.de>
2015-07-26 19:31:55 +0200

    Define custom prefix-map on C-<SPC>
    Usually this would set the mark but it is just a much more comfortable
    keysequence for me then C-c.

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -30,4 +30,9 @@
                           ,load-file-name elapsed)))
             t))
 
+;; define custom leader that I can bind my hydras to
+(defvar my-leader)
+(define-prefix-command 'my-leader)
+(define-key global-map (kbd "C-<SPC>") 'my-leader)
+
 (provide 'init-core)
