8b249ef7e75b688ec621e2befb3d9ce559d00993
Max Linke <max_linke@gmx.de>
2015-07-26 19:33:00 +0200

    Bind hydra to my custom prefix map

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -4,10 +4,9 @@
   :init
   (add-to-list 'evil-emacs-state-modes 'magit-popup-mode)
   (setq magit-last-seen-setup-instructions "1.4.0")
-  (defhydra magit-hydra (global-map "C-x g" :color teal)
+  (defhydra magit-hydra (global-map "C-<SPC> g" :color teal)
     ("s" magit-status)
     ("b" magit-branch-manager)
     ("l" magit-log)))
-
 
 (provide 'init-magit)
