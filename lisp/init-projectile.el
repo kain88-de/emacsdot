5230b233997979c8a837d6e26c73cf2c58254c0f
Max Linke <max_linke@gmx.de>
2015-09-27 23:17:51 +0200

    Remove unused hydras

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -4,21 +4,6 @@
   :commands projectile-global-mode
   :commands helm-projectile-switch-project
   :init
-  (defhydra projectile-hydra ()
-    ("p" helm-projectile-switch-project)
-    ("f" helm-projectile-find-file)
-    ("F" helm-projectile-find-file-in-known-projects)
-    ("T" projectile-find-test-file)
-    ("a" helm-projectile-find-other-file)
-    ("b" helm-projectile-switch-to-buffer)
-    ("S" projectile-save-project-buffers)
-    ("P" projectile-test-project)
-    ("c" max-save-and-build)
-    ("sa" helm-projectile-ack)
-    ("r" projectile-replace)
-    ("I" projectile-ibuffer)
-    ("g" helm-projectile-find-file-dwim))
-  (evil-leader/set-key "p" 'projectile-hydra/body)
   (defun max-save-and-build ()
     "Save and build projects with projectile"
     (interactive)
