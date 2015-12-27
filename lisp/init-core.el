1df3f8dc65c81ac317f555ebdb8445bbcedba734
Max Linke <max_linke@gmx.de>
2015-12-27 23:21:40 +0100

    use https melpa connection

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "https://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 ;; bootstrap use-package
