6b387e33f43735cc7f5c6367bdaeb55d404a1b5d
Max Linke <max_linke@gmx.de>
2016-04-05 23:09:38 +0200

    update elpy to conda installation

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,7 +1,11 @@
 (use-package elpy
   :ensure t
   :defer 1
-  :config (elpy-enable))
+  :config
+  (add-to-list 'exec-path "/home/max/conda/bin")
+  (setq elpy-rpc-python-command "/home/max/conda/bin/python")
+  (setq python-check-command "/home/max/conda/bin/flake8")
+  (elpy-enable))
 
 (use-package py-yapf
   :ensure t)
