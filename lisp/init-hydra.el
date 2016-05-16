b8b3c8731837334c11cb1ca575b3b140967412b9
Max Linke <max_linke@gmx.de>
2016-05-16 07:56:37 +0200

    more hydra changes

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -5,7 +5,7 @@
 (defhydra hydra-compiling (global-map "C-c c")
   " general compiler programming stuff "
   ("n" next-error)
-  ("p" previous-error)
+ ("p" previous-error)
   ("m" moo-complete)
   ("f" fa-show)
   ("q" nil               nil :color blue))
@@ -13,12 +13,14 @@
 (defhydra hydra-org ()
   " org functions access"
   ("a" org-agenda)
-  ("c" org-capture)
+  ("c" org-capture :color blue)
+  ("e" org-export-dispatch :color blue)
   ("j" (lambda ()
          (interactive)
          (find-file "~/org/organizer.org"))))
 ;; overwrite the evil C-o binding that I never use
-(global-set-key (kbd "C-o") 'hydra-org/body)
+(define-key global-map (kbd "C-o") 'hydra-org/body)
+(define-key evil-normal-state-map (kbd "C-o") 'hydra-org/body)
 
 
 (provide 'init-hydra)
