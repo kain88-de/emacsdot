800c84f7223f6f57e1ac21afc79f708832c78507
Max Linke <max_linke@gmx.de>
2016-10-23 17:21:41 +0200

    start using auctex

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -2,6 +2,13 @@
   :ensure t
   :defer t
   :config
-  (setq-default TeX-engine 'pdflatex))
+  (setq-default TeX-engine 'pdflatex)
+  (setq TeX-parse-self t
+        reftex-plug-into-AUCTeX t
+        TeX-PDF-mode t)
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  )
 
 (provide 'init-auctex)
