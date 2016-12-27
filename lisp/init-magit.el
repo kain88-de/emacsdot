b79f6246a3d3aac1e773e124a5f1c96ef571d28b
Max Linke <max_linke@gmx.de>
2016-12-27 11:55:29 +0100

    drop into insert in magit commit

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -5,6 +5,7 @@
   ("C-c g" 'magit-status)
   :config
   (setq magit-last-seen-setup-instructions "1.4.0"
-        magit-visit-ref-behavior '(checkout-any focus-on-ref)))
+        magit-visit-ref-behavior '(checkout-any focus-on-ref))
+  (add-hook 'with-editor-mode-hook 'evil-insert-state))
 
 (provide 'init-magit)
